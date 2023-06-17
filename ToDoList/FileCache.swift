fcf2b99b4957f33c07e283ace23b3e69130fc2b4
FedorBebinov <fsbebinov@edu.hse.ru>
2023-06-17 04:37:29 +0300

    final comit

--- ToDoList/FileCache.swift
+++ ToDoList/FileCache.swift
@@ -9,10 +9,9 @@ import Foundation
 
 class FileCache{
     private (set) var items = [ToDoItem]()
-    private var names: [String] = []
     private let fileName = "json"
     
-    func addTask(newItem: ToDoItem){
+    func addTask(newItem: ToDoItem) {
         for (index, item) in items.enumerated() {
             if item.id == newItem.id {
                 items[index] = newItem
@@ -34,38 +33,38 @@ class FileCache{
         print("task not found") // TODO: throw error
     }
     
-    private func saveNames() {
-            do {
-                let data = try JSONEncoder().encode(names)
-                let fileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent(fileName)
-                try data.write(to: fileURL)
-            } catch {
-                print("Failed to save names: \(error)")
-            }
-        }
-        
-        private func loadNames() {
-            let fileManager = FileManager.default
-            do {
-                let fileURL = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-                    .appendingPathComponent(fileName)
-
-                if fileManager.fileExists(atPath: fileURL.path) {
-                    print("файл по имени json сушествует")
-                    let data = try Data(contentsOf: fileURL)
-                    names = try JSONDecoder().decode([String].self, from: data)
-                    print("loadNames: \(names)")
-                } else {
-                    print("зашли при первом запуске")
-                    names = []
-                    saveNames() // Create a new JSON file with an empty array
-                }
-            } catch {
-                print("Failed to load names: \(error)")
-            }
+    private func saveItems() {
+        do {
+            let data = try JSONEncoder().encode(items)
+            let fileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent(fileName)
+            try data.write(to: fileURL)
+        } catch {
+            print("Failed to save names: \(error)")
         }
     }
-
-    let cache = FileCache()
-    cache.addName("Федор")
+    
+    private func loadItems() {
+        let fileManager = FileManager.default
+        do {
+            let fileURL = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+                .appendingPathComponent(fileName)
+            
+            if fileManager.fileExists(atPath: fileURL.path) {
+                print("файл по имени json сушествует")
+                let data = try Data(contentsOf: fileURL)
+                items = try JSONDecoder().decode([ToDoItem].self, from: data)
+                
+                print("loadNames: \(items)")
+            } else {
+                print("зашли при первом запуске")
+                items = []
+                saveItems() // Create a new JSON file with an empty array
+            }
+        } catch {
+            print("Failed to load names: \(error)")
+        }
+    }
 }
+
+
+
--- ToDoList/ToDoItem.swift
+++ ToDoList/ToDoItem.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-struct ToDoItem{
-    enum Importance {
+struct ToDoItem: Codable {
+    enum Importance: String, Codable {
         case low
         case basic
         case important
@@ -16,7 +16,7 @@ struct ToDoItem{
     
     var id: String = UUID().uuidString
     let text: String
-    let dedline: Date?
+    let deadline: Date?
     let creationDate: Date
     
     var isDone: Bool
@@ -25,6 +25,27 @@ struct ToDoItem{
 }
 
 extension ToDoItem {
+    var json: Any {
+        var items: [String:Any] = [:]
+        items["id"] = id
+        items["text"] = text
+        if importance != Importance.basic{
+            items["importance"] = importance.rawValue
+        }
+        if let deadline{
+            let deadlineFl = CGFloat(deadline.timeIntervalSince1970)
+            items["deadline"] = deadlineFl
+        }
+        items["done"] = isDone
+        let creationFl = CGFloat(creationDate.timeIntervalSince1970)
+        items["created_at"] = creationFl
+        if let modifiedDate{
+            let modifiedFl = CGFloat(modifiedDate.timeIntervalSince1970)
+            items["changed_at"] = modifiedFl
+        }
+        return items
+    }
+
     static func parse (json: Any) -> ToDoItem? {
     
             guard let data = try? JSONSerialization.data(withJSONObject: json, options: []) else {
@@ -42,14 +63,13 @@ extension ToDoItem {
                let deadlineFl = jsonDict["deadline"] as? CGFloat,
                let isDone = jsonDict["done"] as? Bool,
                let creationDateFl = jsonDict["created_at"] as? CGFloat,
-               let modifiedDateFl = jsonDict["changed_at"] as? CGFloat{
+               let modifiedDateFl = jsonDict["changed_at"] as? CGFloat {
                 let creationDate = Date(timeIntervalSince1970: creationDateFl)
                 let modifiedDate = Date(timeIntervalSince1970: modifiedDateFl)
                 let deadline = Date(timeIntervalSince1970: deadlineFl)
-                let todoItem = ToDoItem(id:id, text: text, dedline: deadline, creationDate: creationDate, isDone: isDone, modifiedDate: modifiedDate, importance: importance)
+                let todoItem = ToDoItem(id:id, text: text, deadline: deadline, creationDate: creationDate, isDone: isDone, modifiedDate: modifiedDate, importance: importance)
                 return todoItem
             } else {
-                fatalError("Invalid JSON structure or type mismatch")
                 return nil
             }
     }
